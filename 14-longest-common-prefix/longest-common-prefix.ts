3924c09932a30363895e6f8416e7742569bf276a
setepehen <127885817+setepehen@users.noreply.github.com>
2025-07-10 10:38:18 -0400

    Time: 1 ms (64.65%) | Memory: 55.3 MB (82.88%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.ts
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -1,21 +1,25 @@
 function longestCommonPrefix(strs: string[]): string {
+    // Sort strings to ensure the greatest difference exists between the first and last string value in the array
     let sortedList = strs.sort();
 
+    // Store each character in the first and last strs array values
     const firstEval: string[] = strs[0].split('');
     const lastEval: string[] = strs[strs.length - 1].split('');
 
     let prefix: string = '';
 
 
-        // Traverse first evaluated array value and compare to current array value
+        // Traverse first array value
         for(let i = 0; i < firstEval.length; i++){
+            // Compare it to the last value in the strs array and evaluate if the characters are the same
             if (firstEval[i] == lastEval[i]) {
+                // If so, add the character to the prefix string
                 prefix += firstEval[i];
             } else {
+                // If a match doesn't exist, immediately end the comparison
                 break;
             }
         }
     
-
     return prefix;
 };
