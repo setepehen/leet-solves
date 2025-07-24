2d6fd88e0cc25f9bc4c6ca882e77b6ed0acf08d3
setepehen <127885817+setepehen@users.noreply.github.com>
2025-07-24 15:49:00 -0400

    Time: 17 ms (67.80%) | Memory: 74.4 MB (61.44%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.ts
+++ 217-contains-duplicate/contains-duplicate.ts
@@ -1,16 +1,16 @@
 function containsDuplicate(nums: number[]): boolean {
     
     // define map
-    let numsMap = new Map<number, number>();
+    let numsSet = new Set<number>();
 
     // traverse given array
     for (let i = 0; i < nums.length; i++) {
         // if the map contains the current number, return true
-        if (numsMap.has(nums[i])) {
+        if (numsSet.has(nums[i])) {
             return true
             // if the map doesn't already contain the number, add it to the map i => array value
-        } else if (!numsMap.has(nums[i])) {
-            numsMap.set(nums[i], i);
+        } else {
+            numsSet.add(nums[i]);
         } 
     }
     // return false if no duplicates were found in the map
