58aabbd9baca4695e13ad6007d3f172ad7e84fe3
setepehen <127885817+setepehen@users.noreply.github.com>
2025-09-26 09:29:48 -0400

    Time: 2 ms (61.47%) | Memory: 57 MB (41.83%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -1,22 +1,21 @@
 function twoSum(numbers: number[], target: number): number[] {
     let indicies: number[] = [];
     let firstPointer: number = 0;
-    let secondPointer: number = 0;
+    let secondPointer: number = numbers.length - 1;
+    let sum = 0;
 
-    // Traverse first pointer through array
-    for (let i = 0; i <= numbers.length - 1; i++) {
-        firstPointer = numbers[i];
-        // Traverse second pointer through array
-        for (let j = i + 1; j <= numbers.length - 1; j++){
-            // Start second pointer one value after first pointer each evaluation
-            secondPointer = numbers[j];
-            // If the first pointer value + second pointer value equals target, append index
-            if (firstPointer + secondPointer == target){ 
-                indicies.push(i + 1, j + 1);
+    while (firstPointer < secondPointer) {
+        sum = numbers[firstPointer] + numbers[secondPointer]
+        if (sum == target){
+            indicies.push(firstPointer + 1, secondPointer + 1)
+            return indicies
+        } else {
+            if (target > sum) {
+                firstPointer++
+            } else {
+                secondPointer--
             }
         }
     }
-
-
     return indicies;
 };
