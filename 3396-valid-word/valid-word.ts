de21745e43cb7e0626977358629500ed4b1438dc
setepehen <127885817+setepehen@users.noreply.github.com>
2025-11-19 06:09:04 -0500

    Time: 0 ms (100.00%) | Memory: 55.8 MB (96.97%) - LeetSync

--- 3396-valid-word/valid-word.ts
+++ 3396-valid-word/valid-word.ts
@@ -1,7 +1,11 @@
 function isValid(word: string): boolean {
     
+// Define character classes to check against
+
 let vowelCheck =/^(?=.*[aeiouAEIOU])[A-Za-z0-9]{3,}$/
 let consonantCheck =/[qwrtypsdfghjklzxcvbnm]/i
+
+// Run checks
 
 if (vowelCheck.test(word) && consonantCheck.test(word)) {
     return true
